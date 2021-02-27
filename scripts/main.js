b977821ba5501ce8cbd4faf361c6b0723183bd75
DIALLO Thierno Mamadou <thiernomdou63@gmail.com>
2021-02-27 10:31:44 +0100

    partie1_JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,13 @@
 const CLEAPI = '6fbf739752280f9ef37af35f1534adbf';
 let resultatsAPI;
 
+//récupérer les éléments HTML
+const temps = document.querySelector('.temps');
+const temperature = document.querySelector('.temperature');
+const localisation = document.querySelector('.localisation');
+const heure = document.querySelectorAll('.heure-nom-prevision');
+const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
+
 // on  vérifie si le navigateur a la fonctionnalité géolocation
 if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -42,6 +49,43 @@ function AppelAPI(long, lat) {
         pour récupérer les données au format JSON*/
         .then((data) => {
             console.log(data);
+            resultatsAPI = data;
+
+            //affecter automatiquement nos données API aux HTML
+            temps.innerText = resultatsAPI.current.weather[0].description;
+            temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}°`;
+            localisation.innerText = resultatsAPI.timezone;
+           
+            //les heures, par tranche de trois, avec leur température
+            
+            //récupération de l'heure actuelle en JS
+            let heureActuelle = new Date().getHours();
+
+            //Je parcours le tableau de toutes les heures des paragraphes que j'ai récupére avec mon HTML
+            for(let i = 0; i < heure.length; i++) {
+                //j'incrémente l'heure actuelle de 3 fois après la première itération
+                let heureIncr = heureActuelle + i * 3;
+
+                /*Affichage dans chaque bloc d'heure et  
+                qu'il passe au jour précedent si je dépasse 24h*/
+                if(heureIncr > 24) {
+                    heure[i].innerText = `${heureIncr - 24} h`;
+                } else if(heureIncr === 24) {
+                    heure[i].innerText = "00 h";
+                }
+                else {
+                    heure[i].innerText = `${heureIncr} h`;
+                }
+                
+            }
+
+            /*Je parcours le tableau de tous les temps des paragraphes que j'ai récupére avec mon HTML
+            Affichage dans chaque bloc de temps par tranche de 3 heures
+            */
+            for(let j = 0; j < tempPourH.length; j++) {
+                //hourly, c'est les températures par heure que renvoie l'API
+                tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)} °`;
+            }
         });
        
 }
